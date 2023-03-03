3d6ac84c83164a72f8c32c750ebebe7f7c55ac88
Fatima <yacoubi.fatima@esprit.tn>
2023-03-03 17:11:49 +0100

    Revert "seance 6"
    This reverts commit 170b5b8ab95ccc432834403c536f41ac1a50b983.

--- frontend/src/App/store.js
+++ frontend/src/App/store.js
@@ -2,16 +2,13 @@ import { createStore, combineReducers , applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 //el store houwa objet bch ykounou fih des données partagées bin el components lkol
-import { userLoginReducer } from '../reducers/userReducers'
+
 const reducer = combineReducers({
-    userLogin: userLoginReducer,
-  //reducers
+    //reducers
 })
-const userInfoFromStorage = localStorage.getItem('userInfo')
-? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState ={
-  userLogin: { userInfo : userInfoFromStorage}    
-  //localstorage
+        //localstorage
   }
   const middleware = [thunk]
   
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -4,36 +4,34 @@ import video from "../HeroSection/pottery2.mp4"
 import Button from "../Button/button"
 import {useRef, useState,useEffect} from 'react';
 function Login(){
- 
-  const [ email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const userRef= useRef();
+  const errRef = useRef();
+  const [ user,setUser] = useState('');
+  const [pwd,setPwd] = useState('');
+const[errMsg,setErrMsg] = useState('');
+const [success,setSuccess]= useState(false);
+useEffect(()=>{
+  useRef.current.focus();
+},[])
+useEffect(()=>{
+  setErrMsg('');
+
+},[user,pwd])
 
 
     return(
         <div className='hero-container'>
         <video src={video} autoPlay loop muted />
-        <form onSubmit={submitHandler}>
-          
+        <form >
   <h3 align="center">Login </h3>
 
-  <label for="email">Username</label>
-  <input 
-  
-  type="email" 
-  placeholder="Enter email"
-   id="email"
-   value={email}
-   onChange={(e)=>setEmail(e.target.value)} />
+  <label for="username">Username</label>
+  <input type="text" placeholder="Email or Phone" id="username" />
 
   <label for="password">Password</label>
-  <input type="password"
-   placeholder="Password"
-    id="password" 
-    value={password}
-    onChange={(e)=>setPassword(e.target.value)} 
-    />
+  <input type="password" placeholder="Password" id="password" />
 
-  <Button type="submit" name="Log In"></Button>
+  <Button name="Log In"></Button>
   <div class="social">
     <div class="go"><i class="fab fa-google"></i> Google</div>
     <div class="fb"><i class="fab fa-facebook"></i> Facebook</div>
